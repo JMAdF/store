3ac5390e8201d2f5dacda2222036fc44329c587c
João Marcio <127902520+JMAdF@users.noreply.github.com>
2024-05-01 19:10:44 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+define('APP_NAME',          'PHPSTORE');
+define('APP_VERSION',       '1.0.0');
+
+define('BASE_URL',          'http://localhost:8888/public/index.php');
+
+define('STATUS',            ['PENDENTE','EM PROCESSAMENTO','ENVIADA','CANCELADA','CONCLUIDA']);
+
+// MYSQL
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'php_store');
+define('MYSQL_USER',        'joao');
+define('MYSQL_PASS',        'joao1234');
+define('MYSQL_CHARSET',     'utf8');
+
+// AES encriptação
+define('AES_KEY',           'qs8BzdLD8N7qJgqJ3qmGsuh8HMhCWqG4');
+define('AES_IV',            'WSzH6HcdZAYdQ9be');
+
+// mail
+require_once('/Users/macbookpro/Desktop/email_config.php');
+// define('EMAIL_HOST',        'smtp.gmail.com');
+// define('EMAIL_FROM',        'testeprojetocode@gmail.com');
+// define('EMAIL_PASS',        'teste1234T');
+// define('EMAIL_PORT',        587);
+
+
+
